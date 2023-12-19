6ebcf4953c32498c3784a8f3960acac7fb8aeb59
evans <ekioko404@gmail.com>
2023-12-18 22:26:03 -0800

    fixes

--- routes/root.js
+++ routes/root.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const app = require('../server');
-app.use('/', require('./routes/root'))
-app.use('/payments', require('./routes/payments'))
-app.use('/index', require('./routes/index'))
-
 
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
