4d6f6d5267798a4fbacc876b57691478e85b64bc
evans <ekioko404@gmail.com>
2023-12-23 13:29:28 -0800

    New Model

--- models/User.js
+++ models/User.js
@@ -1,22 +1,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    roles: [{
-        type: String,
-        default: "Employee"
-    }],
-    active: {
-        type: Boolean,
-        default: true
-    }
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true, }, 
+    balance: {type: Number, default: 0},
+    // more data to be added later.
 })
-
-module.exports = mongoose.model('User', userSchema)
